93c37189cfd2b3f2f319447a92a0a41b2647958a
Don Evans <donevans@Dons-Air.attlocal.net>
2023-10-10 05:04:41 -0700

    feat:completed all html, javascript, and css styling for my project

--- app.js
+++ app.js
@@ -3,10 +3,11 @@ const squares = document.querySelectorAll('.square');
 const mole = document.querySelector('.mole');
 const timeLeft = document.querySelector('#time-Left');
 const score = document.querySelector('#score');
+const start = document.querySelector('#start')
 
 let result = 0;
 let hitPosition;
-let currentTime = 10;
+let currentTime = 30;
 let timeId = null;
 
 function randomSquare() {
@@ -23,27 +24,25 @@ randomSquare()
 
 squares.forEach(square => {
     square.addEventListener('mousedown', () => {
-        if(square.id == hitPosition) {
-         result++
-         score.textContent = result;
-         hitPosition = null;
+        if (square.id == hitPosition) {
+            result++
+            score.textContent = result;
+            hitPosition = null;
         }
     })
 })
 function moveMole() {
-    timeId = setInterval(randomSquare, 600)
+    timeId = setInterval(randomSquare, 550)
 }
 moveMole()
 
 function countDown() {
     currentTime--
     timeLeft.textContent = currentTime;
-    if(currentTime === 0) {
+    if (currentTime === 0) {
         clearInterval(countDownTimerId);
         clearInterval(timeId)
-        // alert('Game Over! Your final score is ' + result)
     }
 }
 let countDownTimerId = setInterval(countDown, 1000)
-
 
